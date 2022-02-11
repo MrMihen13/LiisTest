990ba93cb95fd1da3756f6c09eaa0a2ae12fc05c
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-02-11 21:17:37 +0300

    Changer for first deploy

--- liis_test/settings.py
+++ liis_test/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 
