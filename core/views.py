199a9353d7d75532e6ceb4d1877cc465dcd57328
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-02-13 00:14:58 +0300

    Release version 2.0

--- core/views.py
+++ core/views.py
@@ -10,11 +10,39 @@ from core.permissions import IsAuthorUser, IsSubscriberUser, IsOwnerOrReadOnly
 class ArticleListView(generics.ListAPIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = ArticleListSerializer
+    queryset = Article.objects.all()
 
     def get(self, request, *args, **kwargs):
-        articles = Article.objects.all()
 
-        if request.user.role == 'subscriber':
+        articles = self.get_queryset()
+
+        print(request.user.is_authenticated)
+
+        if request.user.is_authenticated and request.user.role == 'subscriber':
+
+            serializer = self.serializer_class(articles, many=True)
+
+            return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        else:
+
+            articles = articles.filter(is_open=True)
+            serializer = self.serializer_class(articles, many=True)
+
+            return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class ArticleView(generics.GenericAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = ArticleListSerializer
+    queryset = Article.objects.all()
+
+    def get(self, request, pk, *args, **kwargs):
+
+        articles = self.get_queryset()
+        articles = articles.filter(id=pk)
+
+        if request.user.is_authenticated and request.user.role == 'subscriber':
 
             serializer = self.serializer_class(articles, many=True)
 
@@ -29,15 +57,24 @@ class ArticleListView(generics.ListAPIView):
 
 
 class ArticleEditApiView(generics.RetrieveUpdateDestroyAPIView):
-    permissions = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
     serializer_class = ArticleEditSerializer
     queryset = Article.objects.all()
 
 
-class ArticleCreateView(generics.ListCreateAPIView):
+class ArticleCreateView(generics.CreateAPIView):
     permission_classes = (permissions.IsAuthenticated, IsAuthorUser)
     serializer_class = ArticleEditSerializer
     queryset = Article.objects.all()
+
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.create(serializer.validated_data, author=request.user)
+
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class AuthorRegistrationView(generics.GenericAPIView):
@@ -52,7 +89,7 @@ class AuthorRegistrationView(generics.GenericAPIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
 
-        return response.Response(serializer.data, status=status.HTTP_200_OK)  # TODO Create redirect to article page
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class SubscriberRegistrationView(generics.GenericAPIView):
@@ -67,4 +104,4 @@ class SubscriberRegistrationView(generics.GenericAPIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
 
-        return response.Response(serializer.data, status=status.HTTP_200_OK)  # TODO Create redirect to article page
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
